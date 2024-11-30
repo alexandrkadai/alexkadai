a5ad7c2ee41c895d430d4a150d67d244037a081a
Alex Kadai <alexandrkadai@gmail.com>
2024-11-30 23:10:54 +0200

    ui fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="mt-10 w-full">
-      <div className="mx-auto h-[300px] w-[350px] items-center justify-center bg-purple-500 text-center">
+      <div className="mx-auto h-[300px] w-[350px] items-center justify-center bg-purple-500 text-center rounded-md">
         <h1 className="text-[32px] font-bold tracking-wider text-black">
           Hello
         </h1>
@@ -12,9 +12,9 @@ export default function Header() {
           </h1>
         </div>
       </div>
-      <div className="text-center uppercase text-purple-700">Front-end Developer React</div>
+      <div className="text-xl w-[350px] m-auto border-2 border-purple-500 mt-2 rounded-md font-bold text-center uppercase text-purple-700">Front-end Developer React</div>
       <div className="text-center">
-    <a className="block m-auto w-[250px] h-[50px] bg-purple-700 p-2 mt-5 uppercase rounded-xl font-bold tracking-wider hover:bg-purple-500" href="/Kadai_Alex_CV.pdf" download >Download My CV</a>
+    <a className="block m-auto w-[350px] h-[50px] bg-purple-700 p-2 mt-5 uppercase rounded-md font-bold tracking-wider hover:bg-purple-500" href="/Kadai_Alex_CV.pdf" download >Download My CV</a>
       </div>
     </div>
   );
